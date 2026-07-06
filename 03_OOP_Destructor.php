588dc54d049ffee81420e720daa60fd894486e17
watid01 <framewatid01@gmail.com>
2026-07-06 15:05:08 +0700

    First commit

---
+++ 03_OOP_Destructor.php
@@ -0,0 +1,18 @@
+<?php
+class Fruit {
+  public $name;
+  public $color;
+
+  function __construct($name, $color) {
+    $this->name = $name;
+    $this->color = $color;
+  }
+
+  function __destruct() {
+    echo "Name: " . $this->name . ". Color: " . $this->color .".<br>";
+  }
+}
+
+$apple = new Fruit('Apple', 'Red');
+$banana = new Fruit('Banana', 'Yellow');
+?>
